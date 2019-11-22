7a3f6f20ffa501086912e958f9834cbc3f44ca54
Kishanbusa123 <58035246+Kishanbusa123@users.noreply.github.com>
2019-11-22 10:15:42 +0530

    Revert "First commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ListsComponent } from './lists/lists.component';
-import { TodayComponent } from './today/today.component';
-import { ScheduledComponent } from './scheduled/scheduled.component';
-import { NewlistsComponent } from './newlists/newlists.component';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { StorageServiceModule } from 'angular-webstorage-service';
-import { TaskComponent } from './task/task.component';
-import { NgxClickToEditModule } from 'ngx-click-to-edit';
-import { PopoverModule } from "ngx-popover";
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ListsComponent,
-    TodayComponent,
-    ScheduledComponent,
-    NewlistsComponent,
-    TaskComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    StorageServiceModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpModule,
-    NgxClickToEditModule.forRoot(),
-    PopoverModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
